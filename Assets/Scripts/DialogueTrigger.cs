25fbf46aae67131e6b23793e178c00994d2114af
ramyzhang <ramyjzh@gmail.com>
2024-03-06 03:34:51 -0500

    added first room dialogue

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public bool wasVisited = false;
 
     public void TriggerDialogue()
     {
+        if (wasVisited)
+        {
+            Debug.Log("Dialogue already visited!");
+            return;
+        }
+        
+        if (GameObject.Find("DialogueCanvas") != null && GameObject.Find("DialogueCanvas").activeSelf)
+        {
+            Debug.Log("Dialogue already active!");
+            return;
+        }
+
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        wasVisited = true;
     }
 }
