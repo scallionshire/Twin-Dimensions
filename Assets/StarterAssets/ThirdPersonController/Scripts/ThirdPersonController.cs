be3965ee53d4b10bb64aeebaa5d26d5f6eb32484
Yumozi <xueeric1223@gmail.com>
2024-03-19 01:08:44 -0400

    Fix cutscenes

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -139,11 +139,11 @@ namespace StarterAssets
             _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<StarterAssetsInputs>();
-#if ENABLE_INPUT_SYSTEM 
-            _playerInput = GetComponent<PlayerInput>();
-#else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
-#endif
+            #if ENABLE_INPUT_SYSTEM 
+                        _playerInput = GetComponent<PlayerInput>();
+            #else
+                        Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
+            #endif
 
             AssignAnimationIDs();
 
