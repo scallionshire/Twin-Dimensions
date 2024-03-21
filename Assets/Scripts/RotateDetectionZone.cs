6c25d72a9d88f038d3ee2c818ac4e98665eac8f3
May <43520497+fatimeh@users.noreply.github.com>
2024-03-21 15:13:22 -0400

    made computer lab changes
    made monitor dual wielding, increase monitor speed as batteries are collected, removed part of the catwalk, adjusted height of platforming battery section

--- Assets/Scripts/RotateDetectionZone.cs
+++ Assets/Scripts/RotateDetectionZone.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class RotateDetectionZone : MonoBehaviour
 {
-    public float rotationSpeed = 50.0f; // Rotation speed in degrees per second
+    public float baseRotationSpeed = 20.0f; 
+    private float currentRotationSpeed; 
+    public float rotationSpeedIncrease = 5.0f; 
+
+    void Start()
+    {
+        currentRotationSpeed = baseRotationSpeed;
+    }
 
     void Update()
     {
-        // Rotate around the up axis of the gameObject
-        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, currentRotationSpeed * Time.deltaTime);
+    }
+
+    public void IncreaseRotationSpeed()
+    {
+        currentRotationSpeed += rotationSpeedIncrease;
+        Debug.Log("Rotation Speed: " + currentRotationSpeed);
     }
 }
