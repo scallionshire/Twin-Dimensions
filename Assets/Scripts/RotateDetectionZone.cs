f0f85ea9ddc24ee91422e9d39b557a21baf4e425
May <43520497+fatimeh@users.noreply.github.com>
2024-03-21 15:35:39 -0400

    monitor freeze during dialogue

--- Assets/Scripts/RotateDetectionZone.cs
+++ Assets/Scripts/RotateDetectionZone.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 
 public class RotateDetectionZone : MonoBehaviour
 {
-    public float baseRotationSpeed = 20.0f; 
-    private float currentRotationSpeed; 
-    public float rotationSpeedIncrease = 5.0f; 
+    public float baseRotationSpeed = 20.0f;
+    private float currentRotationSpeed;
+    public float rotationSpeedIncrease = 5.0f;
+    private DialogueManager dialogueManager; 
 
     void Start()
     {
         currentRotationSpeed = baseRotationSpeed;
+        GameObject gameManagerObject = GameObject.Find("GameManager");
+        if (gameManagerObject != null)
+        {
+            dialogueManager = gameManagerObject.GetComponent<DialogueManager>();
+        }
     }
 
     void Update()
     {
-        transform.Rotate(Vector3.up, currentRotationSpeed * Time.deltaTime);
+        if (dialogueManager != null && !dialogueManager.dialogueActive)
+        {
+            transform.Rotate(Vector3.up, currentRotationSpeed * Time.deltaTime);
+        }
     }
 
     public void IncreaseRotationSpeed()
