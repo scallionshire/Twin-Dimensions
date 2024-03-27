e12c6684d79374dc08373979458f2df2af6ce6c7
ramyzhang <ramyjzh@gmail.com>
2024-03-27 13:25:04 -0400

    increase pull range

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -20,6 +20,109 @@ public class PuzzleManager : MonoBehaviour
     public bool[] puzzlesSolved;
     public PuzzleDataScriptable levelPuzzles;
 
+    void Start()
+    {   
+        // Set player to center of screen
+        GameObject player = GameObject.Find("2D Player");
+        player.transform.position = GameObject.Find("Background").transform.position;
+
+        // if (GameObject.Find("GameManager") != null) {
+        //     gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        //     currentPuzzleId = gameManager.gameState.CurrentPuzzleId;
+        //     switch (gameManager.gameState.CurrentLevel) {
+        //         case Level.tutorial:
+        //             levelPuzzles = gameManager.initTutorialPuzzle;
+        //             break;
+        //         case Level.computerlab:
+        //             levelPuzzles = gameManager.initComputerPuzzle;
+        //             break;
+        //     }
+        // } else {
+        //     currentPuzzleId = -1;
+        // }
+
+        GameObject background = GameObject.Find("Background");
+        background.GetComponent<SpriteRenderer>().sprite = levelPuzzles.backgroundSprite;
+        background.transform.localScale = levelPuzzles.backgroundScale;
+        background.transform.position = levelPuzzles.backgroundPosition;
+        background.GetComponent<SpriteRenderer>().color = levelPuzzles.backgroundColor;
+
+        GameObject rec = GameObject.Find("Rec");
+        rec.transform.localScale = levelPuzzles.recScale;
+        rec.transform.position = levelPuzzles.recPosition;
+
+        for (int i = 0; i < levelPuzzles.wallPositions.Count; i++) {
+            GameObject wall = GameObject.Find("Wall" + i);
+            wall.transform.localPosition = levelPuzzles.wallPositions[i];
+        }
+
+        if (currentPuzzleId == -1) { // map should be blank, no puzzles loaded in
+            Debug.Log("No puzzle selected");
+            return;
+        }
+
+        // for (int i = 0; i < levelPuzzles.puzzles[currentPuzzleId].dialogues.Count; i++) {
+        //     GameObject newDialogueTrigger = Instantiate(levelPuzzles.dialogueTriggerPrefab, levelPuzzles.puzzles[currentPuzzleId].dialogues[i].position, Quaternion.identity);
+        //     newDialogueTrigger.transform.localScale = levelPuzzles.puzzles[currentPuzzleId].dialogues[i].scale;
+        //     newDialogueTrigger.GetComponent<DialogueTrigger>().withUSBDialogue = levelPuzzles.puzzles[currentPuzzleId].dialogues[i].dialogue;
+        // }
+
+        puzzlesSolved = new bool[levelPuzzles.puzzles.Count];
+
+        // Load in puzzle
+        for (int i = 0; i < levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks.Count; i++) {
+            // Instantiate destination blocks
+            GameObject newDestination = Instantiate(levelPuzzles.destinationPrefab, levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].destinationPosition, Quaternion.identity);
+
+            PuzzlePiece piece = new PuzzlePiece() {
+                destinationObject = newDestination,
+                correctSprite = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].correctSprite,
+                isCorrect = false
+            };
+
+            if (levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].isSolved) {
+                newDestination.GetComponent<SpriteRenderer>().sprite = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].correctSprite;
+                piece.isCorrect = true;
+
+                newDestination.name = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].destinationName;
+                newDestination.GetComponent<BlockScript>().blockId = i;
+                newDestination.GetComponent<BlockScript>().blockName = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].blockName;
+            } else {
+                newDestination.GetComponent<SpriteRenderer>().sprite = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].destinationSprite;
+
+                newDestination.name = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].destinationName;
+                newDestination.GetComponent<BlockScript>().blockId = i;
+                newDestination.GetComponent<BlockScript>().blockName = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].blockName;
+
+                // Instantiate movable puzzle blocks
+                GameObject newBlock = Instantiate(levelPuzzles.blockPrefab, levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].blockInitPosition, Quaternion.identity);
+
+                newBlock.GetComponent<SpriteRenderer>().sprite = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].blockSprite;
+                newBlock.name = levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[i].blockName;
+            }
+
+            correctBlocks.Add(piece);
+        }
+
+        for (int k = 0; k < levelPuzzles.puzzles[currentPuzzleId].circuitSprites.Count; k++) {
+            // Instantiate environment sprites
+            GameObject newWire = Instantiate(levelPuzzles.circuitPrefab, levelPuzzles.puzzles[currentPuzzleId].circuitSprites[k].circuitInitPosition, Quaternion.identity);
+
+            if ((k < levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks.Count && levelPuzzles.puzzles[currentPuzzleId].puzzleBlocks[k].isSolved)|| levelPuzzles.puzzles[currentPuzzleId].circuitSprites[k].circuitName.Contains("env")) {
+                newWire.GetComponent<SpriteRenderer>().sprite = levelPuzzles.puzzles[currentPuzzleId].circuitSprites[k].circuitSprite;
+                newWire.name = levelPuzzles.puzzles[currentPuzzleId].circuitSprites[k].circuitName;
+            } else {
+                newWire.GetComponent<SpriteRenderer>().sprite = levelPuzzles.puzzles[currentPuzzleId].circuitSprites[k].circuitSprite;
+                newWire.name = levelPuzzles.puzzles[currentPuzzleId].circuitSprites[k].circuitName;
+
+                Color tmp = newWire.GetComponent<SpriteRenderer>().color;
+                tmp.a = 0.3f;
+                newWire.GetComponent<SpriteRenderer>().color = tmp;
+            }
+        }
+    }
+
     public void LoadPuzzle()
     {   
         correctBlocks.Clear();
