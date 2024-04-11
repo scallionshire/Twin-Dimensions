d616b050346b90dbbc72019dd321a96d9a5f91f1
ramyzhang <ramyjzh@gmail.com>
2024-04-11 15:09:41 -0400

    small tweaks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -631,10 +631,8 @@ public class GameManager : MonoBehaviour
             ThirdPersonController player = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonController>();
             player.enabled = !freeze;
 
-            if (freeze) {
-                Animator playerAnimtor = player.transform.Find("Mesh").gameObject.GetComponent<Animator>();
-                playerAnimtor.SetFloat("Speed", 0);
-            }
+            Animator playerAnimator = player.transform.Find("Mesh").gameObject.GetComponent<Animator>();
+            playerAnimator.SetFloat("Speed", 0);
 
             // Disable player interaction
             Interaction playerInteraction = player.GetComponent<Interaction>();
